e646c1f86b9bf527969af131e6af71a31ed5de5f
Kamila Rojas <camilarleon@gmail.com>
2021-06-15 12:41:18 -0400

    Se crea data y primer componente barra menú

---
+++ src/data/menu.js
@@ -0,0 +1,32 @@
+export default
+{
+  "menu": [
+    {
+
+      "nombre": "Pastel de calabaza",
+      "precio": "5.000"
+    },
+
+    {
+      "nombre": "Empanada de cornualles",
+      "tipo": ["carne", "pollo", "soya"],
+      "precio": "1.500"
+    },
+
+    {
+      "nombre": "Cerveza de mantequilla",
+      "tipo": ["300", "500"],
+      "precio": "2.000"
+    },
+
+    {
+      "nombre": "Píldoras ácidas",
+      "precio": "1.000"
+    },
+
+    {
+      "nombre": "Ranas de chocolate",
+      "precio": "2.000"
+    }
+  ]
+}
